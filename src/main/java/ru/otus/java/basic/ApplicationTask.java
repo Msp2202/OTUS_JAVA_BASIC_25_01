a5738851c2ba898b4dc55a9c4976fb97bd4b1f4f
MSP2202 <msp2202@yandex.ru>
2025-02-24 23:24:21 +0300

    init commit_1

--- src/main/java/ru/otus/java/basic/ApplicationTask.java
+++ src/main/java/ru/otus/java/basic/ApplicationTask.java
@@ -1,4 +1,7 @@
 package ru.otus.java.basic;
 
 public class ApplicationTask{
+    public static void main(String[] args) {
+        System.out.println("Проект с домашними заданиями OTUS Basic 25_01");
+    }
 }
