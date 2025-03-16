b8a70bdd0fb5fe44a248dd383befd4f387cf9570
MSP2202 <msp2202@yandex.ru>
2025-03-16 23:06:53 +0300

    ДЗ ООП Часть 2 наследование и полиморфизм

--- src/main/java/ru/otus/java/basic/OOP/OOP_Task_2/Animal/Animal.java
+++ src/main/java/ru/otus/java/basic/OOP/OOP_Task_2/Animal/Animal.java
@@ -1,31 +1,14 @@
-package ru.otus.java.basic.OOP.OOP_Task_2;
+package ru.otus.java.basic.OOP.OOP_Task_2.Animal;
 
-
-/**
- * класса Animal
- * У каждого животного есть имя, скорость бега и плавания (м/с), и выносливость (измеряется в условных единицах)
- * Затраты выносливости:
- * Все животные на 1 метр бега тратят 1 ед выносливости,
- * Собаки на 1 метр плавания - 2 ед.
- * Лошади на 1 метр плавания тратят 4 единицы
- * Кот плавать не умеет.
- * Собаки на 1 метр плавания - 2 ед.
- * Лошади на 1 метр плавания тратят 4 единицы
- * Кот плавать не умеет.
- * Реализуйте методы run(int distance) и swim(int distance), которые должны возвращать время,
- * затраченное на указанное действие, и “понижать выносливость” животного.
- * Если выносливости не хватает, то возвращаем время -1 и указываем что у животного появилось состояние усталости.
- * При выполнении действий пишем сообщения в консоль.
- * Добавляем метод info(), который выводит в консоль состояние животного.
- */
 public class Animal {
-    private String name;
-    private Integer speedRun;
-    private Integer speedSwim;
-    private Integer endurance;
-    private Integer expensesSwim;
 
-    public Animal(String name, Integer speedRun, Integer speedSwim, Integer endurance, Integer expensesRun, Integer expensesSwim) {
+    protected String name;
+    protected int speedRun;
+    protected int speedSwim;
+    protected int endurance;
+    protected int expensesSwim;
+
+    public Animal(String name, int speedRun, int speedSwim, int endurance, int expensesSwim) {
         this.name = name;
         this.speedRun = speedRun;
         this.speedSwim = speedSwim;
@@ -34,27 +17,34 @@ public class Animal {
     }
 
     public void printInfoEndurance() {
-        System.out.printf("\nЖивотное по имене %s находится в состояние -: %s", name, endurance);
+        System.out.printf("\nЖивотное по имене %s находится в состояние: %s ", name, endurance);
     }
 
     public void swim(int distance) {
-        float time = distance / speedSwim;
-        endurance -= expensesSwim;
-        if(endurance < 0) {
-            System.out.printf("\nЖивотное по имене %s появилась усталость.", name);
-            System.out.println("время -1");
+        if (speedSwim == 0) {
+            System.out.printf("\n%s не умеет плавать!", name);
+            return;
         }
-        System.out.printf("\nЖивотное с именем %s. Проплыли %d за время = %d.", name, distance, time);
+
+        int time = distance / speedSwim;
+        endurance -= (expensesSwim * distance);
+
+        if(endurance < 0) {
+            System.out.printf("\nУ животного по имени %s появилась УСТАЛОСТИ.", name);
+            System.out.print(" время -1");
+        } else {
+            System.out.printf("\nЖивотное с именем %s. Проплыли %d за время = %d. ", name, distance, time);
+        }
     }
 
     public void run(int distance) {
-        float time = distance / speedRun;
+        int time = distance/ speedRun;
         endurance -= distance;
         if(endurance < 0) {
-            printInfoEndurance();
-            System.out.println("время -1");
+            System.out.printf("\nУ животного по имени %s появилась УСТАЛОСТИ.", name);
+            System.out.println(" время -1");
+        } else {
+            System.out.printf("\nЖивотное с именем %s. Пробежали %d за время = %d.", name, distance, time);
         }
-        System.out.printf("\nЖивотное с именем %s. Пробежали %d за время = %d.", name, distance, time);
     }
-
 }
--- src/main/java/ru/otus/java/basic/OOP/OOP_Task_2/Animal/Horse.java
+++ src/main/java/ru/otus/java/basic/OOP/OOP_Task_2/Animal/Horse.java
@@ -1,4 +1,8 @@
-package ru.otus.java.basic.OOP.OOP_Task_2;
+package ru.otus.java.basic.OOP.OOP_Task_2.Animal;
 
-public class Horse {
+public class Horse extends Animal{
+
+    public Horse(String name, int speedRun, int speedSwim, int endurance) {
+        super(name, speedRun, speedSwim, endurance, 4);
+    }
 }
