e195029b7ae33fa0e241cb6cd65fc286042e1b69
MSP2202 <msp2202@yandex.ru>
2025-03-16 23:06:53 +0300

    ДЗ ООП Часть 2 наследование и полиморфизм

--- src/main/java/ru/otus/java/basic/OOP/OOP_Task_2/Animal/Cat.java
+++ src/main/java/ru/otus/java/basic/OOP/OOP_Task_2/Animal/Cat.java
@@ -1,4 +1,9 @@
-package ru.otus.java.basic.OOP.OOP_Task_2;
+package ru.otus.java.basic.OOP.OOP_Task_2.Animal;
 
-public class Cat {
+public class Cat extends Animal{
+
+    public Cat(String name, int speedRun, int endurance) {
+        super(name, speedRun, 0, endurance, 0);
+    }
+
 }
--- src/main/java/ru/otus/java/basic/OOP/OOP_Task_2/Animal/Dog.java
+++ src/main/java/ru/otus/java/basic/OOP/OOP_Task_2/Animal/Dog.java
@@ -1,4 +1,9 @@
-package ru.otus.java.basic.OOP.OOP_Task_2;
+package ru.otus.java.basic.OOP.OOP_Task_2.Animal;
 
-public class Dog{
+
+public class Dog extends Animal{
+
+    public Dog(String name, int speedRun, int speedSwim, int endurance) {
+        super(name, speedRun, speedSwim, endurance, 2);
+    }
 }
